6e09f5af93bf5e127f04b5b85c2802756fe7d49d
SocketFuze <defirence@gmail.com>
2013-08-02 14:09:54 +0100

    changing to deprecated function
    as it works! woo

--- processProxy.php
+++ processProxy.php
@@ -16,9 +16,9 @@ foreach($ips as $ip) {
 	$ip = trim($tmp[0]);
 	$port = trim($tmp[1]);
 	if(ProxyChecker::checkProxy($ip, $port)) {
-		$loc = ProxyChecker::getLocation($ipAddress);
-		$ipAddress = mysql_real_escape_string($ipAddress);
-		$port = mysql_real_escape_string($port);
+		$loc = ProxyChecker::getLocation($ipAddress); 
+		$ipAddress = mysql_escape_string($ipAddress); //FIXME: Update to MySQLi
+		$port = mysql_escape_string($port);
 
 		$db = new DatabaseDriver("localhost", "socket_user", "1a2a3a4a", "socket_proxy");
 		$db->insertProxy($ip, $port, $loc["countryCode"], $loc["countryName"]);
