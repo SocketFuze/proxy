995fa14cae788a55d71fdebcfc015274ff923af3
SocketFuze <defirence@gmail.com>
2013-08-02 18:06:00 +0200

    Modified to Support Date Col

--- table.sql
+++ table.sql
@@ -4,5 +4,6 @@ CREATE TABLE `proxies` (
   `port` int(11) NOT NULL,
   `country_code` varchar(3) NOT NULL,
   `country_name` varchar(255) NOT NULL,
+  `date` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
